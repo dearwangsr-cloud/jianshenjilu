49c4183e6a90beb8f411edf2bc36396deeff7526
Dengke <dearwangsr@gmail.com>
2026-05-14 17:14:28 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+﻿const CACHE_NAME = 'fitness-pro-v1-' + Date.now();
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+// 安装
+self.addEventListener('install', event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 激活
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// 请求策略：网络优先
+self.addEventListener('fetch', event => {
+
+  // HTML：永远优先联网
+  if (event.request.mode === 'navigate') {
+
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const responseClone = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+
+    return;
+  }
+
+  // 其他资源
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
